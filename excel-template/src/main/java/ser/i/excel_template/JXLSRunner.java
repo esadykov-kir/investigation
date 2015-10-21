fad4542d453de1d3f3681092192f6946414d9f8f
esadykov <esadykov@i-novus.ru>
2015-10-21 20:03:12 +0300

    nf Show for jxls. Added deps and start example

---
+++ excel-template/src/main/java/ser/i/excel_template/JXLSRunner.java
@@ -0,0 +1,21 @@
+package ser.i.excel_template;
+
+/**
+ * @author esadykov
+ * @since 21.10.2015 9:57
+ */
+public class JXLSRunner {
+
+    public static void main(String[] args) {
+/*
+        List<Employee> employees = generateSampleEmployeeData();
+        try(InputStream is = ObjectCollectionDemo.class.getResourceAsStream("object_collection_template.xls")) {
+            try (OutputStream os = new FileOutputStream("target/object_collection_output.xls")) {
+                Context context = new Context();
+                context.putVar("employees", employees);
+                JxlsHelper.getInstance().processTemplate(is, os, context);
+            }
+        }
+*/
+    }
+}
